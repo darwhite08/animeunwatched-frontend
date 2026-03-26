9a52f03c800c52be55a64fa06b1f5f7e7c3252fd
darwhite08 <chandrapriyanshu10@gmail.com>
2026-03-26 22:28:21 +0530

    chore: showcase section removal

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,6 +1,5 @@
 import HeroSection from "../../components/layout/HeroSection";
 import LandingFeatures from "@/components/layout/LandingFeatures";
-import Showcase from "@/components/layout/Showcase";
 import CommunityPulse from "@/components/layout/CommunityPulse";
 import FinalCTA from "@/components/layout/FinalCTA";
 import FAQ from "@/components/layout/FAQ";
@@ -11,7 +10,6 @@ export default function HomePage() {
     <LandingFeatures/>
     <FinalCTA/>
     <CommunityPulse/>
-    <Showcase/>
     <FAQ/>
     </main>
   );
